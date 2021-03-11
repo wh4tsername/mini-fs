bc1648d0f1747e5da431183cd29b27c9588aafd8
Denis <pominov.denis@mail.ru>
2021-03-11 20:13:03 +0300

    script fixed, unneeded files deleted

--- test/run.sh
+++ test/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-test_dir_path="/home/wasd/github/mini-fs/test"
-build_path="/home/wasd/github/mini-fs/cmake-build-debug"
-fs_path="/home/wasd/github/mini-fs/fs.bin"
+test_dir_path=${PWD}"/test"
+build_path=${PWD}"/cmake-build-debug"
+fs_path=${PWD}"/fs.bin"
 
-/snap/clion/139/bin/cmake/linux/bin/cmake --build ${build_path} --target mini_fs -- -j 6
-python3 ${test_dir_path}"/write_read_test.py" | ${build_path}"/mini_fs" ${fs_path}
-python3 ${test_dir_path}"/check_write_read.py"
+/snap/clion/139/bin/cmake/linux/bin/cmake --build "${build_path}" --target mini_fs -- -j 6
+python3 "${test_dir_path}""/write_read_test.py" | "${build_path}""/mini_fs" "${fs_path}"
+python3 "${test_dir_path}""/check_write_read.py"
